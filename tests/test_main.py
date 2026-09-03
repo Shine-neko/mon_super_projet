3b46f4a3f6080130496912ce8872f8d11a3e0da5
Mlanawo MBECHEZI <mlanawo.mbechezi@alpacode.io>
2026-09-03 16:49:56 +0200

    unfix test

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,7 +13,7 @@ def client():
 def test_read_root(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Bonjour"}
+    assert response.json() == {"message": "Bonsoir"}
 
 
 def test_health(client):
